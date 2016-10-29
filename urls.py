023eec63fc4f530c7f85840c9299be09e4baf334
Cyrille Gindreau <cyrille@visgence.com>
2016-10-29 15:42:47 -0600

    conformed url patterns to django 1.10, patterns are no longer used

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+import views
 
-urlpatterns = patterns(
-     'chucho.views'
-    ,url(r'(?i)model_editor/(?P<app_name>.+)/(?P<model_name>.+)/$',
-        'model_grid', name='chucho-model-editor')
-    
-    ,url(r'filters/$', 'get_filter_operators', name="chucho-filter-operators")
-    ,url(r'columns/(?P<app_name>.+)/(?P<model_name>.+)/$', 'get_columns', name="chucho-columns")
-    ,url(r'(?P<app_name>.+)/(?P<model_name>.+)/(?P<id>.+)/$', 'api_view', name="chucho-api")
-    ,url(r'(?P<app_name>.+)/(?P<model_name>.+)/$', 'api_view', name="chucho-api")
-)
+urlpatterns = [
+    url(r'(?i)model_editor/(?P<app_name>.+)/(?P<model_name>.+)/$',
+        views.model_grid, name='chucho-model-editor'),
+
+    url(r'filters/$', views.get_filter_operators, name="chucho-filter-operators"),
+    url(r'columns/(?P<app_name>.+)/(?P<model_name>.+)/$', views.get_columns, name="chucho-columns"),
+    url(r'(?P<app_name>.+)/(?P<model_name>.+)/(?P<id>.+)/$', views.api_view, name="chucho-api"),
+    url(r'(?P<app_name>.+)/(?P<model_name>.+)/$', views.api_view, name="chucho-api")
+]
