f4a502c16ed5e24481b1838bcd15155fa0301341
patrickpritchett <patrick@visgence.com>
2025-01-17 09:31:07 -0700

    wip

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 setup(name='chucho',
       version='1.0',
       py_modules=['chucho','slickGrid'],
